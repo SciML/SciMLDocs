d869c941681be38f535b53caf9f87b0198031ff3
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-06-21 06:29:59 -0400

    remove diffeqbayes

--- docs/make.jl
+++ docs/make.jl
@@ -27,7 +27,7 @@ docsmodules = [
                                                           "FEniCS", "DiffEqOperators"],
               "Modeling Tools" => ["ModelingToolkit", "ModelingToolkitStandardLibrary", "Catalyst", 
                                    "NBodySimulator", "ParameterizedFunctions"],
-              "Inverse Problems" => ["DiffEqSensitivity", "DiffEqParamEstim", "DiffEqBayes"],
+              "Inverse Problems" => ["DiffEqSensitivity", "DiffEqParamEstim"],
               "AbstractArray Libraries" => ["RecursiveArrayTools", "LabelledArrays", "MultiScaleArrays"],
               "Uncertainty Quantification" => ["PolyChaos"],
               "Simulation Analysis" => ["GlobalSensitivity"],
